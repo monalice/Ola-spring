398ff820fe12106fc1ff1177472327d0d0ff6c8e
Alice Amorim <alice.amorim@engenharia.ufjf.br>
2023-01-18 14:45:21 -0300

    implements the endpoint

---
+++ src/main/java/com/hello/api/controller/InitController.java
@@ -0,0 +1,15 @@
+package com.hello.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class InitController {
+    
+    @GetMapping
+    public String getHello() {
+        return "Olá Spring!";
+    }
+} 
